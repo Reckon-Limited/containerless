7de6e384330881556c12422847f9b3ac855bb527
Toby Hede <toby.hede@reckon.com>
2016-12-24 06:48:29 +1100

    Add serverless

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 var express = require('express')
 var app = express()
+var handler = require('./handler');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  handler.hello({},{},function(err, result){
+    // res.send('Hello World!')
+    res.send(result)
+  });
+  // res.send('Hello World!')
 })
 
 app.listen(3000, function () {
